bacf2dcfdbed5bebe32eb3b821b6ee7e9166d4f3
meganh@CORP.AMAZONWORKSPACES.COM <meganh@CORP.AMAZONWORKSPACES.COM>
2020-03-23 11:16:27 +1000

    small updates

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { SpinnerOverlay, SpinnerContainer } from "./with-spinner.styles";
+
+const WithSpinner = WrappedComponent => ({ isLoading, ...otherprops }) => {
+    return isLoading ? (
+        <SpinnerOverlay>
+            <SpinnerContainer />
+        </SpinnerOverlay>
+    ) : (
+        <WrappedComponent {...otherprops} />
+    )
+}
+
+export default WithSpinner;
